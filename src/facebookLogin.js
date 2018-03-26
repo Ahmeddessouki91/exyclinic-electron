5931232f212086070e69b0d1140e847a3c02dcd5
Ahmed Dessouki <eng.ahmed.dessouki91@gmail.com>
2018-03-26 18:29:41 +0200

    first commit

---
+++ src/facebookLogin.js
@@ -0,0 +1,53 @@
+const { httpGet, httpPost, showError } = require('./helpers')
+
+function initFacebookLogin(win, FB_APP_ID, URL_PREFIX) {
+
+  console.log('🔐  Facebook Login for', { FB_APP_ID, URL_PREFIX })
+
+  win.webContents.on('did-navigate', (evt, url) => {
+    // intercept login/auth response from facebook
+    if (url.match(/^https\:\/\/www.facebook.com\/connect\/login_success.html#access_token=([^&$]+)/)) {
+      const token = RegExp.$1
+      //console.log('🔐  got fb access token:', token)
+      // get facebook user id (required by openwhyd api)
+      httpGet({ url: `https://graph.facebook.com/me?scope=email&access_token=${token}&fields=first_name,last_name,gender,email` }, (err, res) => {
+        if (err) showError(err)
+        console.log('👱  graph.facebook.com/me =>', err || res)
+        const body = {
+          FName: res.first_name,
+          LName: res.last_name,
+          UserName: res.id,
+          Email: res.email,
+          UserType: Enums.UserType.BusinessUserDoctor,
+          Gender: res.gender == "male" ? 1 : 2,
+          Provider: "Facebook",
+          ProviderValue: res.id
+        }
+        // login to openwhyd using facebook access token and user id
+        // cf
+        httpPost({ url: `${URL_PREFIX}/api/Account/RegisterExternal` }, body, (err, res) => {
+          console.log('🔐  facebookLogin =>', err || res)
+          if (err) {
+            showError(err)
+          } else if (!res.redirect) {
+            showError(res)
+          } else {
+            win.loadURL(URL_PREFIX + res.redirect)
+          }
+          // TODO: better handle errors from res.result, e.g. 'nok, user id=510739408 not found in db'
+        })
+      })
+    }
+  })
+
+  win.webContents.on('new-window', (evt, url) => {
+    if (/^https:\/\/www.facebook.com/.test(url)) {
+      evt.preventDefault()
+      console.log('⚡️  intercepted facebook connect popup:', url)
+      const redirect = 'https://www.facebook.com/connect/login_success.html'
+      win.loadURL(`https://www.facebook.com/v2.10/dialog/oauth?client_id=${FB_APP_ID}&redirect_uri=${redirect}&response_type=token`)
+    }
+  })
+}
+
+exports.initFacebookLogin = initFacebookLogin
---
+++ src/main.js
@@ -0,0 +1,66 @@
+const { app, session, BrowserWindow, Menu } = require('electron')
+const { menu } = require('./menu')
+const { initFacebookLogin } = require('./facebookLogin')
+
+require('electron-debug')({
+  enabled: true, // => DevTools are also usable in production
+  showDevTools: false // `true` to show DevTools on each created BrowserWindow
+})
+
+const TROUBLESHOOTING = false
+const URL_PREFIX = 'https://test.exyclinic.com'
+const FB_APP_ID = 142608023059096
+const BROWSER_WINDOW_SETTINGS = {
+  icon: __dirname + '/icon.ico',
+  width: 1200,
+  height: 900,
+  webPreferences: {
+    nodeIntegration: false, // to let jquery load in web mode
+  },
+}
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let win
+
+function createWindow () {
+  win = new BrowserWindow(BROWSER_WINDOW_SETTINGS)
+  
+  if (TROUBLESHOOTING) {
+    session.defaultSession.clearStorageData([], (data) => {}) // clear cookies and local storage
+    win.webContents.openDevTools()
+  }
+
+  Menu.setApplicationMenu(menu)
+  win.setMenu(menu) // for linux and windows only (necessary?)
+  win.loadURL(URL_PREFIX)
+
+  initFacebookLogin(win, FB_APP_ID, URL_PREFIX)
+
+  win.webContents.on('did-navigate', (evt, url) =>
+    console.log('⚡️  did-navigate', url)
+  )
+
+  // Emitted when the window is closed.
+  win.on('closed', () =>
+    win = null // to allow garbage collection
+  )
+}
+
+// Electron is ready to create browser windows, and APIs can be used
+app.on('ready', createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // On macOS, user quits explicitly the application with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  // On macOS, re-create a window when user clicks the dock icon and no other window is open
+  if (win === null) {
+    createWindow()
+  }
+})
